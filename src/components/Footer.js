e7d0d8f1f1e7cd06d3b82fbda20eca4c74807e9a
Encr1pt0r <presidor16@gmail.com>
2022-06-10 11:29:53 +0100

    Basic layout of footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Footer.scss";
+import { MdOutlineEmail } from 'react-icons/md';
+import { IoLogoLinkedin } from 'react-icons/io';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <>
       <div className='footer'>
-          Footer
+        <h3 className='footer__heading'>Contact Me</h3>
+        <div className='footer__icons'>
+          <MdOutlineEmail className='footer__icons__icon' />
+          <IoLogoLinkedin className='footer__icons__icon' />
+          <FaGithubSquare className='footer__icons__icon' />
+        </div>
+       
       </div>
     </>
   )
