c5fa048d8e73fa4ae88fe23bb3515679c5525a41
Encr1pt0r <presidor16@gmail.com>
2022-06-28 15:30:32 +0100

    Footer icons do not overflow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ import { FaGithubSquare } from 'react-icons/fa';
 export default function Footer() {
   return (
     <>
-      <div className='footer'>
+      <footer className='footer'>
         <h3 className='footer__heading'>Contact Me</h3>
         <div className='footer__icons'>
           <MdOutlineEmail className='footer__icons__icon' />
           <IoLogoLinkedin className='footer__icons__icon' />
           <FaGithubSquare className='footer__icons__icon' />
         </div>
-      </div>
+      </footer>
     </>
   )
 }
